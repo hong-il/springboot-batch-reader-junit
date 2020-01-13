d5f14785de2745430a9bc7f68f646d61fc2e3284
KIM HONGIL <clroyan01@gmail.com>
2020-01-13 19:06:22 +0900

    Create batch junit project

---
+++ src/test/java/boot/batch/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.batch.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
