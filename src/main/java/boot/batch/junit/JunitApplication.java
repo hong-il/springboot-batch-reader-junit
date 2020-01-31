dc59a87115f552f1c209ff40a816ba946e2adf79
KIM HONGIL <clroyan01@gmail.com>
2020-01-31 22:58:59 +0900

    Update main - batch process

--- src/main/java/boot/batch/junit/JunitApplication.java
+++ src/main/java/boot/batch/junit/JunitApplication.java
@@ -1,9 +1,11 @@
 package boot.batch.junit;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class JunitApplication {
 
     public static void main(String[] args) {
